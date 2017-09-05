8863e963140d8469cfb0b976cb239f41c67dfa97
Brian Donnoe <dunow33@hotmail.com>
2017-09-05 02:50:23 -0400

    module 5

--- workoutlogserver/app.js
+++ workoutlogserver/app.js
@@ -5,6 +5,8 @@ app.use('/api/test', function(req, res){
 	res.send("Hello World");
 });
 
+app.use(require('./middleware/headers'));
+
 app.listen(3000, function(){
 	console.log("app is listening on 3000");
 });
