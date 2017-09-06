4b1bc9f9fa124f8e4872a899482dbd23a7ada996
Brian Donnoe <dunow33@hotmail.com>
2017-09-06 13:49:43 -0400

    module 16

--- workoutlogclient/user/auth.js
+++ workoutlogclient/user/auth.js
@@ -36,13 +36,55 @@ $(function(){
 			$("#su_error").text("There was an issue with signup.").show();
 		});
 
-	}
+	},
+
+	login: function() {
+		var username = $("#li_username").val();
+		var password = $("#li_password").val();
+		var user = {
+			user: {
+				username: username,
+				password: password
+			}
+		};
+
+		var login = $.ajax({
+			type: 'POST',
+			url: WorkoutLog.API_BASE + "login",
+			data: JSON.stringify(user),
+			contentType: "application/json"
+		});
+
+		login.done(function(data){
+			if(data.sessionToken){
+				WorkoutLog.setAuthHeader(data.sessionToken);
+			}
+			$("#login-modal").modal("hide");
+			$(".disabled").removeClass("disabled");
+			$("#loginout").text("Logout");
+		}).fail(function(){
+			$("#li_error").text("There was an issue with signup").show();
+		});
+	},
+
 
 		//login method
+
+		loginout: function() {
+			if(window.localStorage.getItem("sessionToken"));
+			$("#loginout").text("Login");
+		}
 		
 		//loginout method
 
 	});
+
+	$("#login").on("click", WorkoutLog.login);
+	$("#loginout").on("click", WorkoutLog.loginout);
+
+	if(window.localStorage.getItem("sessionToken")) {
+		$("#loginout").text("Logout");
+	}
 
 	//bind events
 
