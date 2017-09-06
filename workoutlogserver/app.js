61134c0edac50d34f60a425734965300ce8b89ae
Brian Donnoe <dunow33@hotmail.com>
2017-09-06 10:02:35 -0400

    module 13 adding login signup

--- workoutlogserver/app.js
+++ workoutlogserver/app.js
@@ -9,7 +9,7 @@ User.sync({force:true});
 */
 app.use(bodyParser.json());
 app.use(require('./middleware/headers'));
-//app.use(require('./middleware/validate-session'));
+app.use(require('./middleware/validate-session'));
 app.use('/api/user', require('./routes/user.js'));
 app.use('/api/login', require('./routes/session.js'));
 app.use('/api/test', function(req, res){
