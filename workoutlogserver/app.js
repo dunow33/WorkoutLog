584272cbe7da617c451474dedd86695c967595b8
Brian Donnoe <dunow33@hotmail.com>
2017-09-07 15:16:56 -0400

    module 18 client setup

--- workoutlogserver/app.js
+++ workoutlogserver/app.js
@@ -1,22 +1,25 @@
 require('dotenv').config();
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var sequelize = require('./db.js');
-var User=sequelize.import(__dirname + '//models//user.js');
-//User.sync();/* this will drop (delete) the user table
-sequelize.sync();
-//User.sync({force:true});
+var sequelize = require('./db');
+
+var User = sequelize.import('./models/user');
+
+//Create table
+sequelize.sync(); // sync( {force: true}), to drop then create each time the app starts!
 
 app.use(bodyParser.json());
-app.use(require('./middleware/headers.js'));
-app.use(require('./middleware/validate-session.js'));
-app.use('/api/user', require('./routes/user.js'));
-app.use('/api/login', require('./routes/session.js'));
-app.use('/api/definition', require('./routes/definition.js'));
-app.use('/api/test', function(req, res){
-    res.send("Hello World");
-});
+
+app.use(require('./middleware/headers'));
+app.use(require('./middleware/validate-session'));
+
+
+app.use('/api/user', require('./routes/user'));
+app.use('/api/login', require('./routes/session'));
+app.use('/api/definition', require('./routes/definition'));
+
 app.listen(3000, function(){
-    console.log('App is listening on 3000.')
+	console.log('App is listening on 3000.')
 });
--- workoutlogserver/routes/definition.js
+++ workoutlogserver/routes/definition.js
@@ -5,52 +5,48 @@ var Definition = sequelize.import('../models/definition');
 
 router.post('/', function(req, res) {
 	//variables
-	    var description = req.body.definition.desc;
-        var logType= req.body.definition.type;
-        var owner = req.user.id;
+    var description = req.body.definition.desc;
+    var logType= req.body.definition.type;
+    var owner = req.user.id;
 
-	//methods
-	Definition
-	//objects must match the model 
-	.create({ 
-	   	description: description,
-	   	logType: logType,
-	   	owner: owner
-	   })
-
-		.then(
-				function createSuccess(definition) {
-				//send a response as json
-		   		res.json({
-		   			definition: definition
-		   		});
+	
+	//objects must match the model
+	Definition.create({ 
+		   	description: description,
+		   	logType: logType,
+		   	owner: owner
+		}).then(
+			function createSuccess(definition) {
+			//send a response as json
+	   		res.json({
+	   			definition: definition
+	   		});
 		   	}, 
 		   function createError(err) {
 		       res.send(500, err.message);
-		   }
-
-		);
+		});
 });
 
 router.get('/', function(req, res) {
 	//user variable
 	var userid = req.user.id;
-	Definition
+	
 	//findAll by owner method
-	.findAll({
-		where: { owner: userid }
-	})
-	.then(
-		//success
-		function findAllSuccess(data) {
-			// console.log(data);
-			res.json(data);
-		},
-		//failure
-		function findAllError(err) {
-			res.send(500, err.message);
-		}
-	);
+	Definition
+		.findAll({
+			where: { owner: userid }
+		})
+		.then(
+			//success
+			function findAllSuccess(data) {
+				// console.log(data);
+				res.json(data);
+			},
+			//failure
+			function findAllError(err) {
+				res.send(500, err.message);
+			}
+		);
 });
 
 
